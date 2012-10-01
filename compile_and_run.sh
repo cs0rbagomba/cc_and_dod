ce0d2cd879409caa7a8bca886c807986799f55fc
Denes Matetelki <denes.matetelki@gmail.com>
2012-10-01 17:39:08 +0200

    adding test file, compiler script and lcov generated pngs

---
+++ compile_and_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf *.gcda *.gcno a.out *.gcov lcov.info cov
+gcc -fprofile-arcs -ftest-coverage  tmp.c
+
+./a.out
+
+gcov tmp.c
+cat tmp.c.gcov
+
+
+#lcov --directory . --zerocounters
+#./a.out
+lcov --directory . --capture -o lcov.info
+mkdir cov
+genhtml --frames --legend  --function-coverage  --branch-coverage  --highlight --demangle-cpp  -o ./cov lcov.info
